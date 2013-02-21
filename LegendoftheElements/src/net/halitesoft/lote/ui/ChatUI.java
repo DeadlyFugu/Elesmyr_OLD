1f5c627d7c4b1aafa57c3e177ce8fd5b96571617
DeadlyFugu <deadlyfugu@gmail.com>
2013-02-21 17:11:26 +1100

    Whoops, forgot to include these two classes in the last commit.

---
+++ LegendoftheElements/src/net/halitesoft/lote/ui/ChatUI.java
@@ -0,0 +1,90 @@
+package net.halitesoft.lote.ui;
+
+import net.halitesoft.lote.msgsys.Message;
+import net.halitesoft.lote.msgsys.MessageReceiver;
+import net.halitesoft.lote.player.Camera;
+import net.halitesoft.lote.system.GameClient;
+import net.halitesoft.lote.system.Input;
+import net.halitesoft.lote.system.Main;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class ChatUI implements UserInterface {
+
+	private String msg;
+	private String type;
+	private int sel = 0;
+	private Message recMsg;
+	private int killTimer=-1;
+
+	@Override
+	public void ctor(String extd) {
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg,
+			MessageReceiver receiver) throws SlickException {
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g,
+			Camera cam, GameClient receiver) throws SlickException { //TODO: Make this look good.
+		System.out.println(type);
+		if (type.equals("talk")) {
+			Main.font.drawString(77,17,msg);
+		} else if (type.equals("talkwf")) {
+			Main.font.drawString(77,17,msg.split(":",2)[1]);
+		} else if (type.equals("ask")) {
+			Main.font.drawString(77,17,msg.split("\\|")[0]);
+			for (int i=0; i< msg.split("\\|").length-1; i++) {
+				if (i==sel) {
+					Main.font.drawString(346, 17+i*16, ">");
+				}
+				Main.font.drawString(356,17+i*16,msg.split("\\|")[i+1].split(":",2)[1]);
+			}
+		} else {
+			Main.font.drawString(77,17,"ERROR: ChatUI type = "+type);
+		}
+		if (killTimer>0) {
+			killTimer--;
+			if (killTimer==0) {
+				receiver.ui.remove(this);
+			}
+		}
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, GameClient receiver) {
+		org.newdawn.slick.Input in = gc.getInput();
+		if (Input.isKeyPressed(gc, "int")) {
+			if (type.equals("ask"))
+				recMsg.reply(recMsg.getSender()+".tresponse", msg.split("\\|")[sel+1].split(":")[0], null);
+			if (type.equals("talkwf"))
+				recMsg.reply(recMsg.getSender()+".tresponse", msg.split(":")[0], null);
+			killTimer=10;
+		}
+		if (in.isKeyPressed(org.newdawn.slick.Input.KEY_UP) && type.equals("ask"))
+			if (sel>0)
+				sel--;
+		if (in.isKeyPressed(org.newdawn.slick.Input.KEY_DOWN) && type.equals("ask"))
+			if (sel<msg.split("\\|").length-2)
+				sel++;
+	}
+
+	@Override
+	public boolean blockUpdates() {
+		return true;
+	}
+
+	public void setMsg(String type, String data, Message recMsg) {
+		this.type=type;
+		this.msg=data;
+		this.recMsg=recMsg;
+		sel=0;
+		killTimer=-1;
+	}
+
+}
---
+++ LegendoftheElements/src/net/halitesoft/lote/world/entity/EntityTalk.java
@@ -0,0 +1,63 @@
+package net.halitesoft.lote.world.entity;
+
+import net.halitesoft.lote.msgsys.Message;
+import net.halitesoft.lote.msgsys.MessageReceiver;
+import net.halitesoft.lote.msgsys.MessageSystem;
+import net.halitesoft.lote.player.Camera;
+import net.halitesoft.lote.system.GameClient;
+import net.halitesoft.lote.world.Region;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.minlog.Log;
+
+public class EntityTalk extends Entity {
+	Image spr;
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg, MessageReceiver receiver)
+			throws SlickException {
+		spr = new Image("data/ent/"+extd.split(",",2)[0]+".png",false,0);
+	}
+	
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g,
+			Camera cam, GameClient receiver) throws SlickException {
+		if (spr==null)
+			init(gc,sbg,receiver);
+		spr.draw(xs+cam.getXOff(),ys+cam.getYOff());
+	}
+	@Override
+	public void interact(Region region, EntityPlayer entityPlayer, MessageReceiver receiver, Connection connection) {
+		response("int",connection);
+	}
+	
+	@Override
+	public void receiveMessageExt(Message msg, MessageReceiver receiver) {
+		if (msg.getName().equals("tresponse")) {
+			response(msg.getData(),msg.getConnection());
+		} else {
+			Log.warn("EntityPlayer ingored message "+msg);
+		}
+	}
+
+	private void response(String call, Connection connection) {
+		if (call.equals("int")) {
+			MessageSystem.sendClient(this,connection,new Message("CLIENT.talk","ask:How are you?|good:Good.|bad:Bad.|meh:Meh."),false);
+		} else if (call.equals("good"))  {
+			MessageSystem.sendClient(this,connection,new Message("CLIENT.talk","talk:That's great!"),false);
+		} else if (call.equals("bad"))  {
+			MessageSystem.sendClient(this,connection,new Message("CLIENT.talk","talkwf:again:Aww :("),false);
+		} else if (call.equals("meh"))  {
+			MessageSystem.sendClient(this,connection,new Message("CLIENT.talk","talk:Oh, okay."),false);
+		} else if (call.equals("again")) {
+			MessageSystem.sendClient(this,connection,new Message("CLIENT.talk","ask:Are you still bad?|bad:Yes.|good:No."),false);
+		} else {
+			Log.warn("Unrecognised tresponse call '"+call+"'");
+		}
+	}
+}
