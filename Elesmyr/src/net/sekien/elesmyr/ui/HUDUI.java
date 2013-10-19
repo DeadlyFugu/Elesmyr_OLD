db75b48d750d8c19578d143355b49bd956e3d7ea
Lachie <alachie@sekien.net>
2013-10-19 12:16:31 +1100

    Massive changes to the in game UI

--- Elesmyr/src/net/sekien/elesmyr/ui/HUDUI.java
+++ Elesmyr/src/net/sekien/elesmyr/ui/HUDUI.java
@@ -6,7 +6,6 @@
 
 package net.sekien.elesmyr.ui;
 
-import net.sekien.elesmyr.Element;
 import net.sekien.elesmyr.msgsys.MessageEndPoint;
 import net.sekien.elesmyr.player.Camera;
 import net.sekien.elesmyr.system.FontRenderer;
@@ -21,162 +20,161 @@ import org.newdawn.slick.*;
 import java.util.ArrayList;
 
 public class HUDUI implements UserInterface {
-private Image bart;
-private Image barb;
-private Image bars;
-private Image equip;
-private SpriteSheet elements;
-private int updtimer = 10;
-private ArrayList<EntityPlayer> nearplayers;
+    private int updtimer = 10;
+    private ArrayList<EntityPlayer> nearplayers;
 
-private boolean inited = false;
+    private boolean inited = false;
 
-@Override
-public boolean inited() {
-	return inited;
-}
+    private Image bg;
+    private Image itembg;
+    private Image bgstart;
+    private Image bgend;
+    private Image barbg;
+    private Image redglow;
+    private Image greenglow;
+    private Image blueglow;
+	private Image bars;
 
-public HUDUI() {
-	nearplayers = new ArrayList<EntityPlayer>();
-}
+    @Override
+    public boolean inited() { return inited; }
+    public HUDUI() { nearplayers = new ArrayList<EntityPlayer>(); }
 
-@Override
-public void init(GameContainer gc,
-                 MessageEndPoint receiver) throws SlickException {
-	inited = true;
-	bart = FileHandler.getImage("ui.bar_t");
-	barb = FileHandler.getImage("ui.bar_b");
-	bars = FileHandler.getImage("ui.bars");
-	equip = FileHandler.getImage("ui.equip");
-	elements = new SpriteSheet(FileHandler.getImage("ui.elements"), 16, 16);
-}
+    @Override
+    public void init(GameContainer gc, MessageEndPoint receiver) throws SlickException {
+         inited = true;
+             bg = FileHandler.getImage("ui.hud.bg");
+        itembg  = FileHandler.getImage("ui.hud.itembg");
+        bgstart = FileHandler.getImage("ui.hud.bgstart");
+          bgend = FileHandler.getImage("ui.hud.bgend");
+          barbg = FileHandler.getImage("ui.hud.barbg");
+        redglow = FileHandler.getImage("ui.hud.redglow");
+      greenglow = FileHandler.getImage("ui.hud.greenglow");
+       blueglow = FileHandler.getImage("ui.hud.blueglow");
+	       bars = FileHandler.getImage("ui.hud.bars");
+    }
 
-@Override
-public void render(Renderer renderer, Camera cam, GameClient receiver) throws SlickException {
-	Graphics g = renderer.g;
-	bart.draw(0, 0, Main.INTERNAL_RESX, 16);
-	FontRenderer.drawString(0, 0, HUDUI.getTopString(), g);
-	barb.draw(0, Main.INTERNAL_RESY-64, Main.INTERNAL_RESX, 64);
+    @Override
+    public void render(Renderer renderer, Camera cam, GameClient receiver) throws SlickException {
+        Graphics g = renderer.g;
+        int dy = Main.INTERNAL_RESY-64;
+        int dx = 0;
 
-	int i = 0;
-	if (nearplayers.size() > 1)
-		for (EntityPlayer ep : nearplayers) {
-			int dx = i*160;
-			int dy = Main.INTERNAL_RESY-64;
-			equip.draw(dx, dy);
-			if (ep.pdat.getEquipped()!=null)
-				ep.pdat.getEquipped().getItem().spr.draw(dx+16, dy+16);
-			FontRenderer.drawString(dx+60, dy+6, ep.getName(), g);
+        for(int i = 0; i < 1+Main.INTERNAL_RESX/480; i++) {
+            bg.draw(i*480, dy, 480, 64);
+        }
+
+        bgstart.draw(0, dy, 12 ,64);
+        bgend.draw(Main.INTERNAL_RESX-12, dy, 12,64);
+
+        for(int i = 0; i < 5; i++){
+            itembg.draw(11+i*47, dy+16);
+        }
+
+        if (nearplayers.size() > 0) {
+            EntityPlayer ep = nearplayers.get(0);
+            FontRenderer.drawStringHud(dx+246, dy+27, ep.getName(), ep.getElement().color(), g);
+
+            if (ep.pdat.getEquipped()!=null) {
+                ep.pdat.getEquipped().getItem().spr.draw(dx+14, dy+19);
+            }
+
+			//render bars
+			for(int i = 0; i < 3; i++){
+				barbg.draw(Main.INTERNAL_RESX-115,dy+13+i*16,104,10);
+			}
+
+			    int StartX = Main.INTERNAL_RESX-113;
+			    int StartY = dy+15;
+			      int health = (int)(((float) ep.pdat.health/ep.pdat.healthMax)*100);
+			     int magicka = (int)(((float) ep.pdat.magicka/ep.pdat.magickaMax)*100);
+			     int stamina = (int)(((float) ep.pdat.stamina/ep.pdat.staminaMax)*100);
+
+			FontRenderer.drawStringPixel(Main.INTERNAL_RESX-126-FontRenderer.getPixelWidth( String.valueOf(health) + ":"),dy+14,String.valueOf(health) + ":",new Color(0xdd4632),g);
+			FontRenderer.drawStringPixel(Main.INTERNAL_RESX-126-FontRenderer.getPixelWidth( String.valueOf(stamina) + ":"),dy+30,String.valueOf(stamina) + ":",new Color(0x80e24a), g);
+			FontRenderer.drawStringPixel(Main.INTERNAL_RESX-126-FontRenderer.getPixelWidth( String.valueOf(magicka) + ":"),dy+46,String.valueOf(magicka) + ":",new Color(0x5aa3ee), g);
+
+			if (health == 100) {
+				redglow.draw(Main.INTERNAL_RESX-118,dy+10,110,16);
+			}
+			if (stamina == 100) {
+				greenglow.draw(Main.INTERNAL_RESX-118,dy+26,110,16);
+			}
+            if (magicka == 100) {
+                blueglow.draw(Main.INTERNAL_RESX-118,dy+42,110,16);
+            }
+
+			int healthmid = health -4;
+            int staminamid = stamina -4;
+            int magickamid = magicka -4;
+
 			bars.startUse();
-			bars.drawEmbedded(dx+57, dy+25, dx+57+104, dy+34, 0, 75, 104, 84);
-			bars.drawEmbedded(dx+57, dy+35, dx+57+104, dy+44, 0, 75, 104, 84);
-			bars.drawEmbedded(dx+57, dy+45, dx+57+104, dy+54, 0, 75, 104, 84);
-			if (ep.pdat!=null) {
-				bars.drawEmbedded(dx+57, dy+25, dx+57+((float) ep.pdat.health/ep.pdat.healthMax)*104, dy+34, 0, 48, ((float) ep.pdat.health/ep.pdat.healthMax)*104, 57);
-				bars.drawEmbedded(dx+57, dy+35, dx+57+((float) ep.pdat.magicka/ep.pdat.magickaMax)*104, dy+44, 0, 57, ((float) ep.pdat.magicka/ep.pdat.magickaMax)*104, 65);
-				bars.drawEmbedded(dx+57, dy+45, dx+57+((float) ep.pdat.stamina/ep.pdat.staminaMax)*104, dy+54, 0, 65, ((float) ep.pdat.stamina/ep.pdat.staminaMax)*104, 74);
+
+			int capwidth = 2;
+			int healthdis = healthmid + capwidth;
+            int staminadis = staminamid + capwidth;
+            int magickadis = magickamid + capwidth;
+
+			if(health > 0){
+				bars.drawEmbedded(StartX, StartY, StartX+capwidth, StartY+6, 0, 0, capwidth, 6);
+				bars.drawEmbedded(StartX+capwidth, StartY, StartX+healthdis, StartY+6, 2, 0, 2, 6);
+				bars.drawEmbedded(StartX+healthdis, StartY, StartX+capwidth+healthdis, StartY+6, 5-capwidth, 0, 5, 6);
+			} else {
+				// renders nothing
 			}
+            if(stamina > 0){
+                bars.drawEmbedded(StartX, StartY+16, StartX+capwidth, StartY+22, 0, 6, capwidth, 12);
+                bars.drawEmbedded(StartX+capwidth, StartY+16, StartX+staminadis, StartY+22, 2, 6, 2, 12);
+                bars.drawEmbedded(StartX+staminadis, StartY+16, StartX+capwidth+staminadis, StartY+22, 5-capwidth, 6, 5, 12);
+            } else {
+                // renders nothing
+            }
+            if(magicka > 0){
+                bars.drawEmbedded(StartX, StartY+32, StartX+capwidth, StartY+38, 0, 12, capwidth, 18);
+                bars.drawEmbedded(StartX+capwidth, StartY+32, StartX+magickadis, StartY+38, 2, 12, 2, 18);
+                bars.drawEmbedded(StartX+magickadis, StartY+32, StartX+capwidth+magickadis, StartY+38, 5-capwidth, 12, 5, 18);
+            } else {
+                // renders nothing
+            }
 			bars.endUse();
-			i++;
 		}
-	else if (nearplayers.size()==1) {
-		int dx = 0;
-		int dy = Main.INTERNAL_RESY-64;
-		EntityPlayer ep = nearplayers.get(0);
-		equip.draw(dx, dy);
-		if (ep.pdat.getEquipped()!=null)
-			ep.pdat.getEquipped().getItem().spr.draw(dx+16, dy+16);
-		FontRenderer.drawString(dx+62, dy+25, ep.getName(), ep.getElement().color(), g);
-		//drawElement(dx+64, dy+45, ep.getElement());
-		//FontRenderer.drawString(dx+62, dy+45, "("+ep.getElement().toString()+")", g);
-		int bdx = Main.INTERNAL_RESX-112;
-		bars.startUse();
-		//bars.drawEmbedded(bdx, dy+13, bdx+104, dy+25, 0, 75, 104, 84);
-		bars.drawEmbedded(bdx, dy+13, bdx+104, dy+25, 0, 36, 104, 48);
-		bars.drawEmbedded(bdx, dy+26, bdx+104, dy+38, 0, 36, 104, 48);
-		bars.drawEmbedded(bdx, dy+39, bdx+104, dy+51, 0, 36, 104, 48);
-		bars.drawEmbedded(bdx, dy+13, bdx+((float) ep.pdat.health/ep.pdat.healthMax)*104, dy+25, 0, 0, ((float) ep.pdat.health/ep.pdat.healthMax)*104, 12);
-		bars.drawEmbedded(bdx, dy+26, bdx+((float) ep.pdat.magicka/ep.pdat.magickaMax)*104, dy+38, 0, 12, ((float) ep.pdat.magicka/ep.pdat.magickaMax)*104, 24);
-		bars.drawEmbedded(bdx, dy+39, bdx+((float) ep.pdat.stamina/ep.pdat.staminaMax)*104, dy+51, 0, 24, ((float) ep.pdat.stamina/ep.pdat.staminaMax)*104, 36);
-		//bars.drawEmbedded(bdx, dy+13, bdx+(ep.pdat.health/60f)*104, dy+25, 0, 0, (ep.pdat.health/60f)*104, 12);
-		bars.endUse();
-	}
-	//playerInfo.draw(Main.INTERNAL_RESX*0.5f-80,Main.INTERNAL_RESY-64);
-}
+    }
 
-private void drawElement(int x, int y, Element element) {
-	switch (element) {
-		case FIRE:
-			elements.getSprite(0, 0).draw(x, y);
-			break;
-		case WATER:
-			elements.getSprite(1, 0).draw(x, y);
-			break;
-		case EARTH:
-			elements.getSprite(2, 0).draw(x, y);
-			break;
-		case AIR:
-			elements.getSprite(3, 0).draw(x, y);
-			break;
-		case NEUTRAL:
-			elements.getSprite(0, 1).draw(x, y);
-			break;
-		case VOID:
-			elements.getSprite(1, 1).draw(x, y);
-			break;
-	}
-}
 
-private static String getTopString() {
-	//FontRenderer.drawString(0, 0, "#LotE |"+Main.version+": "+(CLIENT?"|$bar.client| ":"")+(SERVER?"|$bar.server| ":"")+(MessageSystem.fastLink?"|$bar.fastlink| ":""), g);
-	return "#$bar.title| |"+Main.version/*+":"+
-			       (MessageSystem.CLIENT?" |$bar.client|":"")+
-			       (MessageSystem.SERVER?" |$bar.server|":"")+
-			       (MessageSystem.fastLink?" |$bar.fastlink|":"")+
-			       (Globals.get("debug", false)?" |$menu.debug|":"")*/;
-}
-
-@Override
-public void update(GameContainer gc, GameClient receiver) {
-	if (updtimer==0) { //Calculate nearest players
-		updtimer = 60;
-		if (receiver.player.region==null)
-			return; //Do nothing if the PlayerClient.region is null.
-		EntityPlayer player = ((EntityPlayer) receiver.player.region.entities.get(receiver.player.entid));
-		if (player==null)
-			return; //Do nothing if the player is null. The player temp. goes null when switching regions.
-		nearplayers.clear();
-		ArrayList<EntityPlayer> temp = new ArrayList<EntityPlayer>();
-		for (Entity e : receiver.player.region.entities.values()) {
-			if (e instanceof EntityPlayer)
-				temp.add((EntityPlayer) e);
-		}
-		int tsize = Math.min(temp.size(), 4);
-		for (int i = 0; i < tsize; i++) {
-			EntityPlayer nearest = null;
-			float ndist = 256; //Max distance. Players past here will be ignored
-			for (Entity e : temp) {
-				float tnd;
-				if ((tnd = (float) Math.hypot(e.x-player.x, e.y-player.y)) < ndist) {
-					nearest = (EntityPlayer) e;
-					ndist = tnd;
-				}
-			}
-			if (nearest!=null) {
-				nearplayers.add(nearest);
-				temp.remove(nearest);
-			}
-		}
-	} else {
-		updtimer--;
-	}
-}
-
-@Override
-public boolean blockUpdates() {return false;}
-
-@Override
-public void ctor(String extd) {
-}
-
+    @Override
+    public void update(GameContainer gc, GameClient receiver) {
+        if (updtimer==0) { //Calculate nearest players
+            updtimer = 60;
+            if (receiver.player.region==null)
+                return; //Do nothing if the PlayerClient.region is null.
+            EntityPlayer player = ((EntityPlayer) receiver.player.region.entities.get(receiver.player.entid));
+            if (player==null)
+                return; //Do nothing if the player is null. The player temp. goes null when switching regions.
+            nearplayers.clear();
+            ArrayList<EntityPlayer> temp = new ArrayList<EntityPlayer>();
+            for (Entity e : receiver.player.region.entities.values()) {
+                if (e instanceof EntityPlayer)
+                    temp.add((EntityPlayer) e);
+            }
+            int tsize = Math.min(temp.size(), 4);
+            for (int i = 0; i < tsize; i++) {
+                EntityPlayer nearest = null;
+                float ndist = 256; //Max distance. Players past here will be ignored
+                for (Entity e : temp) {
+                    float tnd;
+                    if ((tnd = (float) Math.hypot(e.x-player.x, e.y-player.y)) < ndist) {
+                        nearest = (EntityPlayer) e;
+                        ndist = tnd;
+                    }
+                }
+                if (nearest!=null) {
+                    nearplayers.add(nearest);
+                    temp.remove(nearest);
+                }
+            }
+        } else { updtimer--; }
+    }
+    @Override
+    public boolean blockUpdates() {return false;}
+    @Override
+    public void ctor(String extd) {}
 }
