40676c75398770598bda87aa330c228b8cac33dd
Lachie <alachie@sekien.net>
2013-10-19 12:12:48 +1100

    Added the ability to skip intro

--- Elesmyr/src/net/sekien/pepper/IntroState.java
+++ Elesmyr/src/net/sekien/pepper/IntroState.java
@@ -14,84 +14,86 @@ import org.newdawn.slick.Image;
 
 import java.awt.*;
 
-/**
- * Created with IntelliJ IDEA. User: matt Date: 27/04/13 Time: 6:07 PM To change this template use File | Settings |
- * File Templates.
- */
 public class IntroState extends Node {
-private Image sekien;
-private Image game;
-int time = 0;
+    private boolean skipintro = Globals.get ("skipIntro", false);
+    private Image sekien;
+    private Image game;
+    int time = 0;
 
-public IntroState(String name) {
-	super(name);
-	try {
-		sekien = FileHandler.getImage("menu.intro1"); //Sekien
-		game = FileHandler.getImage("menu.intro3"); //Elesmyr
-	} catch (SlickException e) {
-		e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-	}
-}
+    public IntroState(String name) {
+        super(name);
+        try {
+            sekien = FileHandler.getImage("menu.intro1"); //Sekien
+            game = FileHandler.getImage("menu.intro2"); //Elesmyr
+        } catch (SlickException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 
-private static final float stepTime = 100;
+    private static final float stepTime = 100;
 
-@Override
-public void render(Renderer renderer, int w, int h, boolean sel) {
-	float bx, by, bw, bh;
-	bw = sekien.getWidth()/2;
-	bh = sekien.getHeight()/2;
-	bx = (w-bw)/2;
-	by = (h-bh)/2;
-	renderer.fillRect(Color.black, bx, by, bw, bh);
-	if (time < stepTime*0.5) {
-		sekien.setAlpha(time/(stepTime*0.5f));
-		sekien.draw(bx, by, bw, bh);
-	} else if (time < stepTime*2.5f) {
-		sekien.setAlpha(1);
-		sekien.draw(bx, by, bw, bh);
-	} else if (time < stepTime*3f) {
-		sekien.draw(bx, by, bw, bh);
-		game.setAlpha((time-stepTime*2.5f)/(stepTime*0.5f));
-		game.draw(bx, by, bw, bh);
-	} else if (time < stepTime*5f) {
-		game.setAlpha(1);
-		game.draw(bx, by, bw, bh);
-	} else if (time < stepTime*5.5f) {
-		game.setAlpha(1-((time-stepTime*5f)/(stepTime*0.5f)));
-		game.draw(bx, by, bw, bh);
-	} else {
-		StateManager.forcePop();
-		StateManager.setStateInitial("Main");
-	}
-	if (Globals.get("debug", false)) renderer.text(0, 16, Float.toString((float) Math.floor((time/stepTime)*10)/10));
-}
 
-@Override
-public void update(GameContainer gc) {
-	time++;
-}
+    @Override
+    public void render(Renderer renderer, int w, int h, boolean sel) {
+        if(skipintro){
+            time = 10*(int) stepTime;
+        }
 
-@Override
-public Node nodeAt(int x, int y) {
-	return this;
-}
+        float bx, by, bw, bh;
+        bw = sekien.getWidth()/2;
+        bh = sekien.getHeight()/2;
+        bx = (w-bw)/2;
+        by = (h-bh)/2;
+        renderer.fillRect(Color.black, bx, by, bw, bh);
+        if (time < stepTime*0.5) {
+            sekien.setAlpha(time/(stepTime*0.5f));
+            sekien.draw(bx, by, bw, bh);
+        } else if (time < stepTime*2.5f) {
+            sekien.setAlpha(1);
+            sekien.draw(bx, by, bw, bh);
+        } else if (time < stepTime*3f) {
+            sekien.draw(bx, by, bw, bh);
+            game.setAlpha((time-stepTime*2.5f)/(stepTime*0.5f));
+            game.draw(bx, by, bw, bh);
+        } else if (time < stepTime*5f) {
+            game.setAlpha(1);
+            game.draw(bx, by, bw, bh);
+        } else if (time < stepTime*5.5f) {
+            game.setAlpha(1-((time-stepTime*5f)/(stepTime*0.5f)));
+            game.draw(bx, by, bw, bh);
+        } else {
+            StateManager.forcePop();
+            StateManager.setStateInitial("Main");
+        }
+        if (Globals.get("debug", false)) renderer.text(0, 16, Float.toString((float) Math.floor((time/stepTime)*10)/10));
+    }
 
-@Override
-public void setSel(int x, int y) {
-}
+    @Override
+    public void update(GameContainer gc) {
+        time++;
+    }
 
-@Override
-public void onAction(Action action) {
-	if (action.equals(Action.SELECT))
-		time = 5*(int) stepTime;
-	else if (action.equals(Action.LEFT))
-		time -= 11;
-	else if (action.equals(Action.RIGHT))
-		time += 9;
-}
+    @Override
+    public Node nodeAt(int x, int y) {
+        return this;
+    }
 
-@Override
-public Dimension getDimensions(boolean sel) {
-	return new Dimension(640, 480);
-}
+    @Override
+    public void setSel(int x, int y) {
+    }
+
+    @Override
+    public void onAction(Action action) {
+        if (action.equals(Action.SELECT))
+            time = 5*(int) stepTime;
+        else if (action.equals(Action.LEFT))
+            time -= 11;
+        else if (action.equals(Action.RIGHT))
+            time += 9;
+    }
+
+    @Override
+    public Dimension getDimensions(boolean sel) {
+        return new Dimension(640, 480);
+    }
 }
